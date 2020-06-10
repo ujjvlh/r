76396ee7c7cb000eadbe22f03231c206edfde57c
142434 <utkars.rajput@gmail.com>
2020-06-10 15:09:10 +0530

    2

--- 1.py
+++ 1.py
@@ -1,14 +1,22 @@
 import os
-def b(fn,d):
-	f=open(fn+".html"
-f0=open("index.html","w")
-f0.write("<html>\n<body>\n")
+def b(fn):
+	f=open(fn+".html","w")
+	f.write("<html>\n<body style=\"word-spacing:-.25em;\">\n")
+	if os.path.isfile("../"+fn):
+		fi=open("../"+fn,"r")
+		html=fi.read()
+		f.write("<audio controls playb id=\"ac\"><source src=\""+html[html.find('https'):html.find('mp3')+3]+"\" type=\"audio/mpeg\"></audio>")
+		f.write(html[html.find('</div>')+6:].replace('\n','<br>\n').replace(' ',''))
+		f.write("<script>ac.playbackRate=0.5</script>")
+		fi.close()
+	else:
+		os.mkdir(fn)
+		for d in sorted(os.listdir("../"+fn)):
+			if d=="_index.md":continue
+			f.write("<a href=\""+fn.split('/')[-1]+"/"+d+".html\">"+d+"</a><br>\n")
+			b(fn+"/"+d)
+		f.write("</body>\n</html>")
+	f.close()
+b("AndhrapAThaH")
 #../AndhrapAThaH/1_bAlakANDam/02-putrakAmaH/005_City_ayodhya_detailed.md
-for d1 in sorted(os.listdir("AndhrapAThaH/")):
-	if d1=="_index.md":continue
-	f0.write("<a href=\"AndhrapAThaH/"+d1+".html>"+d1+"</a><br>\n")
-	f1=open("AndhrapAThaH/"+d1+".html")
-	f1.write("<html>\n<body>\n")
-	f1.close()
-f0.write("</body>\n</html>")
-f0.close()
+
--- 2/index.html
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-2
-</body>
-</html>
